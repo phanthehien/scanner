af85c564b025c3f74beb2bb9cb86bf7057ecd58f
phanthehien <hien@linecorp.com>
2020-03-17 18:43:50 +0700

    add docker-compose

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ const router = require('./router.js');
 const scanResult = require('./scan-result');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 8080;
 let db = null;
 let server;
 
