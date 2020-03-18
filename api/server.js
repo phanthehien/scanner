55323234eca9a11d2e82c8e67b44d15acd787e6e
Hien <phanthehien@gmail.com>
2020-03-19 00:25:05 +0700

    add docker file

--- api/server.js
+++ api/server.js
@@ -3,8 +3,8 @@ const http = require('http');
 const router = require('./router.js');
 const scanResult = require('./scan-result');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = '0.0.0.0';
+const port = 8080;
 let db = null;
 let server;
 
